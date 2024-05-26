7764ed20f947628988931075cb52588d16c94f60
aka-abdol <thegrolam@gmail.com>
2024-05-26 09:04:19 +0330

    feat: detail page done

--- detail-page/main.py
+++ detail-page/main.py
@@ -9,25 +9,29 @@ import os
 
 # Add the parent directory of 'common' to the sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from common.getters import getTexts, getTextByIndex
+from common.getters import getTexts, getTextByIndex, getAttribute, getText
 
 # from selenium.webdriver.support import expected_conditions as EC
 
-def getDetail(element: WebElement):
+def getDetail(element: WebDriver):
+    headerSectionElement = element.find_element(By.TAG_NAME, "h1")
+    episodesSectionElement = element.find_element(By.ID, "browse-episodes-season").find_element(By.XPATH, '../../../../../..')
     return {
-        "duration": getTextByIndex(element, By.CLASS_NAME, "ipc-title__text", 4),
+        "duration": headerSectionElement.find_element(By.XPATH, '..').find_elements(By.TAG_NAME, "li")[3].text, 
         "genres": getTexts(
-            element.find_element(By.CLASS_NAME, "ipc-chip-list__scroller"),
+            headerSectionElement.find_element(By.XPATH, '../../..').find_element(By.CLASS_NAME, "ipc-chip-list__scroller"),
             By.CLASS_NAME,
             "ipc-chip__text",
         ),
         "stars": getTexts(
-            element.find_element(
+            headerSectionElement.find_element(By.XPATH, '../../..').find_element(
                 By.CLASS_NAME, "ipc-metadata-list-item__content-container"
             ),
             By.CLASS_NAME,
-            "ipc-metadata-list-item__list-content-item",
+            "ipc-metadata-list-item__list-content-item--link",
         ),
+        "seasons": getAttribute(element.find_element(By.ID, "browse-episodes-season"), "aria-label").split()[0],
+        "episodes": getText(episodesSectionElement, By.CLASS_NAME, "ipc-title__subtext")
     }
 
 
@@ -35,7 +39,7 @@ url = "https://www.imdb.com/title/tt10541088/?ref_=sr_t_1"
 driver = webdriver.Chrome()
 driver.get(url)
 driver.implicitly_wait(10)
-print(getDetail(driver.find_element(By.CLASS_NAME, "ipc-page-content-container")))
+print(getDetail(driver))
 # df = pd.DataFrame(dataset)
 # df.to_csv("./out_final2.csv")
 driver.quit()
