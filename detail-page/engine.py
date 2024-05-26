507df58b8865b0368d69edc11a0443e618644c00
aka-abdol <thegrolam@gmail.com>
2024-05-26 12:31:06 +0330

    feat: detail v1 done

---
+++ detail-page/engine.py
@@ -0,0 +1,54 @@
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support.wait import WebDriverWait
+import sys
+import os
+
+# Add the parent directory of 'common' to the sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from common.getters import getTexts, getTextByIndex, getAttribute, getText
+
+# from selenium.webdriver.support import expected_conditions as EC
+
+def getDetail(element: WebDriver):
+    headerSectionElement = element.find_element(By.TAG_NAME, "h1")
+    episodesSectionElement = element.find_element(By.ID, "browse-episodes-season").find_element(By.XPATH, '../../../../../..')
+    reviewCardElement = element.find_elements(By.CLASS_NAME, "ipc-icon--more-vert")[1].find_element(By.XPATH, '../../../../..')
+    return {
+        "duration": headerSectionElement.find_element(By.XPATH, '..').find_elements(By.TAG_NAME, "li")[-1].text, 
+        "genres": getTexts(
+            headerSectionElement.find_element(By.XPATH, '../../..').find_element(By.CLASS_NAME, "ipc-chip-list__scroller"),
+            By.CLASS_NAME,
+            "ipc-chip__text",
+        ),
+        "stars": getTexts(
+            headerSectionElement.find_element(By.XPATH, '../../..').find_element(
+                By.CLASS_NAME, "ipc-metadata-list-item__content-container"
+            ),
+            By.CLASS_NAME,
+            "ipc-metadata-list-item__list-content-item--link",
+        ),
+        "seasons": getAttribute(element.find_element(By.ID, "browse-episodes-season"), "aria-label").split()[0],
+        "episodes": getText(episodesSectionElement, By.CLASS_NAME, "ipc-title__subtext"),
+        "review": getText(reviewCardElement, By.CLASS_NAME, "ipc-html-content-inner-div")
+    }
+
+def getDetailByLink(url: str):
+    driver = webdriver.Chrome()
+    driver.get(url)
+    driver.implicitly_wait(10)
+    detail = getDetail(driver)
+    driver.quit()
+    return detail
+
+# url = "https://www.imdb.com/title/tt10541088/?ref_=sr_t_1"
+# driver = webdriver.Chrome()
+# driver.get(url)
+# driver.implicitly_wait(10)
+# print(getDetail(driver))
+# df = pd.DataFrame(dataset)
+# df.to_csv("./out_final2.csv")
+# driver.quit()
--- detail-page/main.py
+++ detail-page/main.py
@@ -1,45 +1,15 @@
 import pandas as pd
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.remote.webelement import WebElement
-from selenium.webdriver.remote.webdriver import WebDriver
-from selenium.webdriver.support.wait import WebDriverWait
-import sys
-import os
+from engine import getDetailByLink
 
-# Add the parent directory of 'common' to the sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from common.getters import getTexts, getTextByIndex, getAttribute, getText
+csv_dir = input('list of items in csv dir: ')
+df = pd.read_csv(csv_dir)
 
-# from selenium.webdriver.support import expected_conditions as EC
+detail_data = []
+for i in range(5):
+    item = df.loc[i]
+    print(item)
+    url = item['link']
+    detail_data.append(getDetailByLink(url))
 
-def getDetail(element: WebDriver):
-    headerSectionElement = element.find_element(By.TAG_NAME, "h1")
-    episodesSectionElement = element.find_element(By.ID, "browse-episodes-season").find_element(By.XPATH, '../../../../../..')
-    return {
-        "duration": headerSectionElement.find_element(By.XPATH, '..').find_elements(By.TAG_NAME, "li")[3].text, 
-        "genres": getTexts(
-            headerSectionElement.find_element(By.XPATH, '../../..').find_element(By.CLASS_NAME, "ipc-chip-list__scroller"),
-            By.CLASS_NAME,
-            "ipc-chip__text",
-        ),
-        "stars": getTexts(
-            headerSectionElement.find_element(By.XPATH, '../../..').find_element(
-                By.CLASS_NAME, "ipc-metadata-list-item__content-container"
-            ),
-            By.CLASS_NAME,
-            "ipc-metadata-list-item__list-content-item--link",
-        ),
-        "seasons": getAttribute(element.find_element(By.ID, "browse-episodes-season"), "aria-label").split()[0],
-        "episodes": getText(episodesSectionElement, By.CLASS_NAME, "ipc-title__subtext")
-    }
-
-
-url = "https://www.imdb.com/title/tt10541088/?ref_=sr_t_1"
-driver = webdriver.Chrome()
-driver.get(url)
-driver.implicitly_wait(10)
-print(getDetail(driver))
-# df = pd.DataFrame(dataset)
-# df.to_csv("./out_final2.csv")
-driver.quit()
+detailed_df = pd.concat([df, pd.DataFrame(detail_data)], axis=1)
+detailed_df.to_csv('out_detailed.csv', index=False)
